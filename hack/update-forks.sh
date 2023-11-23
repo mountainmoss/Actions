10ae53281ba1d32f786bca1f0e01bb847d83af8c
Moss Cantwell <m.cantwell@gns.cri.nz>
2023-11-23 14:26:02 +1300

    workaround lack of native associative data in bash

--- hack/update-forks.sh
+++ hack/update-forks.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gh api --method POST -f -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/$1/merge-upstream" -f branch="$2"
+REPO=$(echo "$1" | cut -d, -f1)
+BRANCH=$(echo "$1" | cut -d, -f2)
+gh api --method POST -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" -f branch="$BRANCH" "/repos/$REPO/merge-upstream"
